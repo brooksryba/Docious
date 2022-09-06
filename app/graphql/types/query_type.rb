875ec40e11893d39342733a0920c229ad835aa5a
Brooks Ryba <brooksryba@gmail.com>
2022-09-05 20:11:47 -0400

    Add user model, graphql, rubocop

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :users, [Types::UserType], null: false,
+                                     description: 'Return a list of users'
+
+    def users
+      User.all
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.delete_all
+User.create!([
+               email: 'poppins@gmail.com',
+               first_name: 'Marry',
+               last_name: 'Poppins'
+             ])
