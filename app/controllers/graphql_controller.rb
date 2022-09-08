1ad70bc24437a9f38ec0050ac0180b7d356743b7
Brooks Ryba <brooksryba@gmail.com>
2022-09-08 19:21:26 -0400

    lint

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -26,7 +26,7 @@ class GraphqlController < ApplicationController
   # gets current user from token stored in the session
   def current_user # rubocop:disable Metrics/AbcSize
     # if we want to change the sign-in strategy, this is the place to do it
-    return unless request.headers['Authorization'] && request.headers['Authorization'] != ""
+    return unless request.headers['Authorization'] && request.headers['Authorization'] != ''
 
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
     token = crypt.decrypt_and_verify request.headers['Authorization']
